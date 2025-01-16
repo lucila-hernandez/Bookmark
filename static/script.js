0c43811ba1e574a8ae7ef2b6ca1a2c5c0d4cd65b
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-01-16 14:50:03 -0800

    bookmark

--- static/script.js
+++ static/script.js
@@ -1,8 +1,19 @@
+// Function to toggle book details (show/hide)
 function toggleDetails(bookId) {
     let details = document.getElementById(bookId);
     if (details.style.display === "none") {
         details.style.display = "block"; 
     } else {
         details.style.display = "none"; 
+    }
+}
+
+// Function to toggle the "More Information" section (show/hide)
+function toggleMoreInfo(infoId) {
+    let info = document.getElementById(infoId);
+    if (info.style.display === "none") {
+        info.style.display = "block"; 
+    } else {
+        info.style.display = "none"; 
     }
 }
